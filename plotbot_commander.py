switched to single buffer that holds timings for both motors, used by a single timer. This ensures correct movement but calculation of the timings is more complicated. Proof of concept with constant timings is working.

plotbot_commander.py
@@ -43,11 +43,6 @@ class Form(QtWidgets.QDialog):
         rightlayout.addWidget(self.runfile_button)
 
 
-        self.demo_button = QtWidgets.QPushButton("Demo")
-        self.demo_button.setEnabled(False)
-        rightlayout.addWidget(self.demo_button)
-
-
         rightlayout.addWidget(QtWidgets.QLabel("#Steps:"))   
         self.step_spin = QtWidgets.QSpinBox()
         self.step_spin.setRange(0, 3000)
@@ -160,8 +155,6 @@ class Form(QtWidgets.QDialog):
         self.jog_b_right_button.clicked.connect(self.jog_b_right)
         self.jog_b_left_button.clicked.connect(self.jog_b_left)
 
-        self.demo_button.clicked.connect(self.demo)
-
         self.move_button.clicked.connect(self.move)
         self.togglemotors_button.clicked.connect(self.togglemotors)
         self.motors_enabled = False
@@ -189,7 +182,6 @@ class Form(QtWidgets.QDialog):
             self.down_button.clicked.connect(self.bot.pen_down)
 
             self.home_button.setEnabled(True)
-            #self.demo_button.setEnabled(True)
             self.jog_a_left_button.setEnabled(True)
             self.jog_a_right_button.setEnabled(True)
             self.jog_b_left_button.setEnabled(True)
@@ -284,7 +276,6 @@ class Form(QtWidgets.QDialog):
 
     def move(self):
         self.bot.start_moving()
-        self.demo_button.setEnabled(True)
 
 
     def runfile(self):
@@ -297,51 +288,31 @@ class Form(QtWidgets.QDialog):
         
         self.bot.start_moving()
 
-        a,b = pu.read_tmng_files("./Zeichnung")
-
-        if len(a) > 0:
-            a_finished = False
-        else:
-            a_finished = True
-        if len(b) > 0:
-            b_finished = False
-        else:
-            b_finished = True
+        t = pu.read_tmng_files("./Zeichnung.tmng")
 
         size = 500
 
-        a_ind = 0
-        b_ind = 0
-        while not (a_finished and b_finished):
+        finished = False
+
+        ind = 0
+        while not finished:
             
-            alen, blen = self.bot.read_bufferlength()
+            l = self.bot.read_bufferlength()
 
-            if alen is not None:
-                if (((alen <= blen) and (not a_finished)) or b_finished):
-                    if alen < (3000-size):
-                        send = a[a_ind:a_ind+size,:]
-                        if len(send) > 0:
-                            self.bot.write_buffer(send[:,0]*1e-6, np.array(send[:,1],dtype=np.int), b'a')
-                            a_ind += size
-                            #print("alen ",alen," send b ",send.shape[0])
-                        else:
-                            a_finished = True  
-
-                elif not b_finished:
-                    if blen < (3000-size):
-                        send = b[b_ind:b_ind+size,:]
-                        if len(send) > 0:
-                            self.bot.write_buffer(send[:,0]*1e-6, np.array(send[:,1],dtype=np.int), b'b')
-                            b_ind += size
-                            #print("blen ",blen," send a ",send.shape[0])
-                        else:
-                            b_finished = True  
-
+            if l is not None:
+                if l < (3000-size):
+                    send = t[ind:ind+size,:]
+                    if len(send) > 0:
+                        self.bot.write_buffer(send[:,0]*1e-6, np.array(send[:,1],dtype=np.int))
+                        ind += size
+                        #print("alen ",alen," send b ",send.shape[0])
+                    else:
+                        finished = True  
 
             time.sleep(0.02)
 
-        while alen > 0 or blen > 0:
-            alen, blen = self.bot.read_bufferlength()
+        while l > 0:
+            l = self.bot.read_bufferlength()
             time.sleep(0.05)
         self.moveto()
 
@@ -417,40 +388,6 @@ class Form(QtWidgets.QDialog):
 
 
             time.sleep(0.01)
-
-
-    def demo(self):
-        #xpos, ypos = self.bot.read_positions()
-
-        size = 500
-
-        count = 0
-        ta = 0
-        tb = 0
-        while count < 49:
-
-            #print(self.bot.read_positions())
-            alen, blen = self.bot.read_bufferlength()
-            #print(count,"buffer length",xlen,ylen)
-
-            if alen is not None:
-                if alen <= blen:
-                    if alen < (3000-size):
-                        timings = pu.generate_sine_movement(np.arange(start=ta,stop=ta+size))
-                        #timings = pu.generate_sine_movement(np.arange(start=tx,stop=tx+size))
-                        ta += size
-                        self.bot.write_buffer(timings, np.repeat(1,len(timings)), b'a')
-                        
-                else:
-                    if blen < (3000-size):
-                        timings = pu.generate_sine_movement(np.arange(start=tb,stop=tb+size),phase=np.pi)
-                        #timings = pu.generate_sine_movement(np.arange(start=ty,stop=ty+size),phase=np.pi)
-                        tb += size
-                        self.bot.write_buffer(timings, np.repeat(1,len(timings)), b'b')
-                        count += 1
-
-            time.sleep(0.01)
-
 
 
     def __del__(self):

plotbot_utils.py
@@ -56,9 +56,8 @@ def generate_triangle_movement(t, freq = 0.00008, phase = 0.0,dtmin = 0.0001, dt
 
 
 def read_tmng_files(filename):
-    a = np.loadtxt(filename+"_a.tmng",delimiter=" ",skiprows=1)
-    b = np.loadtxt(filename+"_b.tmng",delimiter=" ",skiprows=1)
-    return a, b
+    return np.loadtxt(filename,delimiter=" ",skiprows=1)
+     
 
 
 class PlotBot(object):
@@ -154,10 +153,9 @@ class PlotBot(object):
         self.write(reply)
 
 
-    def write_buffer(self, timings, actions, axis):
+    def write_buffer(self, timings, actions):
         reply = b''
         reply += b'b'
-        reply += axis
         reply += bytes(struct.pack('<l',len(timings)))
         for i in range(len(timings)):
             reply += bytes(struct.pack("<f", timings[i]))
@@ -184,10 +182,9 @@ class PlotBot(object):
 
         if len(msg) > 1:
             if msg[0] == ord("l"): 
-                xlen = struct.unpack('<l',msg[2:6])[0]
-                ylen = struct.unpack('<l',msg[7:11])[0]
-                return xlen, ylen
-        return None, None
+                l = struct.unpack('<l',msg[1:5])[0]
+                return l
+        return None
 
 
     def read_positions(self):
@@ -230,8 +227,6 @@ class PlotBot(object):
 if __name__ == '__main__':
     #bot = PlotBot()
     #bot.home()
-    a,b = read_tmng_files("./Zeichnung")
+    a = read_tmng_files("./Zeichnung")
     print(a.shape)
     print(a)
-    print(b.shape)
-    print(b)